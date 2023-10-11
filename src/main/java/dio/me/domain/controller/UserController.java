975e2cdfd39a456479722cac4b3617b51204e79f
Adriel Alexander <adriel.alexs123@gmail.com>
2023-10-11 16:51:00 -0300

    Tentativa de publicação no Railway

--- src/main/java/dio/me/domain/controller/UserController.java
+++ src/main/java/dio/me/domain/controller/UserController.java
@@ -2,7 +2,7 @@ package dio.me.domain.controller;
 
 import dio.me.domain.model.User;
 import dio.me.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
